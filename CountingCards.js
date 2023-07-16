1df88a26a502d24226c3928aaecc2f6540b2bc16
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-16 18:37:34 +0100

    New Commit

---
+++ CountingCards.js
@@ -0,0 +1,34 @@
+let count = 0;
+
+function CountingCards(card) {
+switch (card) {
+ case 2:
+ case 3:
+ case 4:
+ case 5:
+ case 6:
+    count ++;
+break;
+
+ case 10:
+ case "J":
+ case "Q":
+ case "K":
+ case "A":
+    count --;
+break;
+
+}
+
+if(count > 0){
+return count + " Bet";
+}
+else{
+  return count + " Hold";
+}
+
+  
+
+}
+
+console.log(CountingCards(8));
