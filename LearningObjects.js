ff2eadf2daf13a8be07dfb4ce5814b5dc2b3df54
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-17 00:35:51 +0100

    Objects

---
+++ LearningObjects.js
@@ -0,0 +1,12 @@
+const Male={
+name: "Khan",
+age: 17,
+state: "Lagos",
+country: "Nigeria",
+complexion: 'dark',
+course: "ADSE"
+}
+
+const getMale=("My Name is " + Male.name + ". I am " + Male.age + ". I reside in " + Male.state + " which is in " + Male.country + ". I am " + Male.complexion + " in complexion" + ". I study " + Male.course + " at Aptech Nigeria");
+
+console.log(getMale);
