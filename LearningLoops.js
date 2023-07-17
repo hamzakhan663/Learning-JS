49516bf1c58708ae984db793f0a1f200749bd61c
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-17 10:20:51 +0100

    Loops and Arrays

---
+++ LearningLoops.js
@@ -0,0 +1,22 @@
+//Initialize an empty array
+const ourArray = [];
+//Initial value of variable "i"
+let i = 0;
+// Execute 5 times and append numbers 0 through 20 to "ourArray"
+while(i <= 20){
+    ourArray.push(i);
+    i++;
+}
+// Display the array
+console.log(ourArray);
+
+// Initialize variable "value" to delete first element of "ourArray"
+const value = ourArray.shift();
+// Display deleted element
+console.log(value);
+// Initialize variable "result" to delete last element of "ourArray"
+const result = ourArray.pop();
+// Display deleted element
+console.log(result);
+
+
