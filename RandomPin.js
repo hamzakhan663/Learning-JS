4add8a3cf96d435fdec9377d74edcff35d8f8af0
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-18 10:40:47 +0100

    Modified Changes

--- RandomPin.js
+++ RandomPin.js
@@ -3,4 +3,4 @@ function Random(num,num2){
     num = Math.floor(num);
     return Math.floor(Math.random() * (num-num2) + num2);
 }
-console.log("Your New PIN is "+ Random(1500,10000) + ". Do not Share⚠️");
+console.log("Your New PIN is " + Random(1500,10000) + ". Do not Share⚠️");
