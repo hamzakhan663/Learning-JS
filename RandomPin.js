aa8a77a73a7bba87d4c745889f1ece2456a9eb1b
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-18 10:38:36 +0100

    New File

---
+++ RandomPin.js
@@ -0,0 +1,6 @@
+function Random(num,num2){
+    num2 = Math.ceil(num2);
+    num = Math.floor(num);
+    return Math.floor(Math.random() * (num-num2) + num2);
+}
+console.log("Your New PIN is "+ Random(1500,10000) + ". Do not Share⚠️");
