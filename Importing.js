032696f5fcbd07d53eafa71964de86393a227754
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-21 00:35:54 +0100

    New file

---
+++ Importing.js
@@ -0,0 +1,6 @@
+// Import Function
+import {uppercaseString,lowercaseString} from './string_functions.js'; 
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
