add71b264b9027bf7b1bb61352de4f898255f659
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-21 00:04:09 +0100

    New File

---
+++ Exporting.js
@@ -0,0 +1,10 @@
+//Exporting Functions
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+  
+  const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
+  
+  export{uppercaseString,lowercaseString};
