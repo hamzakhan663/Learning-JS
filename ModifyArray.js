24aed88ac72f9dfae850bbdeed008515ec1cdd2c
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-22 00:14:24 +0100

    New Files

---
+++ ModifyArray.js
@@ -0,0 +1,14 @@
+//Splice
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const spliced = arr.splice(2, 3, 19, 20);
+
+console.log(arr);
+
+//Slice
+
+const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const sliced = arr2.slice(2, 5);
+
+console.log(sliced);
