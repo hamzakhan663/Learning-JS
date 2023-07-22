15a9a3e9542bc111dc2846333b404f7823c3babc
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-22 06:29:31 +0100

    New File

---
+++ CopyArray.js
@@ -0,0 +1,13 @@
+// Copy an array with the spread operator
+function copyMachine(arr, num) {
+    let newArr=[];
+    while (num >= 1) {
+      
+  newArr.push([...arr]);
+    
+      num--;
+    }
+    return newArr;
+  }
+  
+  console.log(copyMachine([true, false, true], 2));
