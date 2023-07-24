0bd61c9cf58977336396ccd9432900252a36103e
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-24 19:23:29 +0100

    New File

---
+++ RefactorGlobalVar.js
@@ -0,0 +1,22 @@
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+
+function add(arr,bookName) {
+let newArr = [...arr]; // Copy the bookList array to a new array.
+  newArr.push(bookName);
+  return newArr;
+ 
+  
+}
+
+
+function remove(arr,bookName) {
+ let newArr = [...arr]; // Copy the bookList array to a new array.
+  if (newArr.indexOf(bookName) >= 0) {
+    // Check whether the bookName parameter is in new array.
+    newArr.splice(newArr.indexOf(bookName), 1); // Remove the given paramater from the new array.
+    return newArr; // Return the new array.
+  }
+    
+    }
