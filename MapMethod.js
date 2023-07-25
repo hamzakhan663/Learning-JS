dd56eb2cd9f8f432a4400ccad24a928d11bf2d5b
hamzakhan663 <sulaimonhamza233@gmail.com>
2023-07-25 19:43:30 +0100

    New File

---
+++ MapMethod.js
@@ -0,0 +1,11 @@
+const AnimeList = [
+    {name:"Attack on Titan" , year: 2013},
+    {name: "Black Clover", year: 2019},
+    {name:"Naruto", year: 2022},
+    {name: "One Piece", year: 1999},
+    {name: "The Legend of Zelda", year: 1992},
+    {name: "The Legend of Zelda: Breath of the Wild", year: 1993},
+    {name: "Dragon Ball Z", year: 2000},
+];
+    const names = AnimeList.map(user => user.name);
+    console.log(names);
